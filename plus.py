6c95c0d027da850c02b95279623586396d893d8c
Leonid Bolshakov <boleoa@gmail.com>
2023-09-23 14:35:08 +0800

    'Time_display_brightness'

--- plus.py
+++ plus.py
@@ -1,81 +1,29 @@
-from accessify import private, protected
-from math import sqrt
-import pytest
+from time import sleep
 
-class Point:
 
-#    @protected
-    def check_coord(self, coord):
-        if not type( coord ) in (int, float):
-            raise Exception( f'The {coord} coordinate is not a number.' )
+def calc(func):
+    it_happened_before = dict()
+
+    def wrapper(*args):
+        if it_happened_before.get(args) is None:
+            res = func(*args)
+            it_happened_before[args] = res
         else:
-            return coord
-
-    def __init__(self, x, y):
-
-        self.__x = self.check_coord(x)
-        self.__y = self.check_coord(y)
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, x):
-       self.__x = self.check_coord(x)
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, y):
-       self.__y = self.check_coord(y)
-
-    def __str__(self):
-        return f'{self.__x = }, {self.__y = }'
-
-def dist_point(p1: Point, p2: Point) -> float:
-    return sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2)
-
-p1 = Point(3.2, -3)
-p2 = Point(0,4)
-
-pytest.raises(Exception, Point, "3", 1)
-pytest.raises(Exception, Point, 3, "1")
-assert dist_point(Point(3.2, -3), Point(0,4)) == 7.696752561957543
-p1 = Point(0, 1)
-p1.x = 3
-p1.y = 4
-assert str(p1) == 'self.__x = 3, self.__y = 4'
-
-import time
-
-#for i in range(11):
-#    time.sleep(1)
-#    print(time.time(), datetime.datetime.now())
-
-class MySleep:
-    def __init__(self, sec):
-        self.__zero_time__ = time.time()
-        self.sec = sec
-
-    def end_request(self, stage):
-        return time.time() <= self.__zero_time__ + self.sec * stage
-
-my_sleep = MySleep(sec=1)
-for i in range(11):
-    while(my_sleep.end_request(stage=i+1)):
-        time.sleep(0.001)
-    print(time.time())
+            res = it_happened_before[args]
+        return res
+    return wrapper
 
 
+@calc
+def ppp(param_i, param_j):
+    print('***')
+    sleep(1)
+    return param_i + param_j
 
 
-
-
-
-
-
-
-
+for i in range(2):
+    for j in range(3):
+        print(ppp(i, j))
+for i in range(2):
+    for j in range(3):
+        print(ppp(i, j))
--- timer_1.1.py
+++ timer_1.1.py
@@ -13,67 +13,72 @@
 #                                                      #
 ########################################################
 import time
-import datetime
 from os import path, remove, walk
 from sys import exit as sys_exit
+from threading import Thread
 
-import customtkinter as CTKi
+import customtkinter as ctki
 from playsound import playsound
 from gtts import gTTS
-from threading import Thread
 from num2words import num2words
 
 from TimTK import TimerTK
 import constant as c
 
+
 class MySleep:
     def __init__(self, sec):
-        self.__zero_time__ = time.time()
-        self.__sec__ = sec
-        self.__seconds_passed__ = sec
+        self.__zero_time = time.time()
+        self.__sec = sec
+        self.__seconds_passed = sec
 
     def end_of_cycle(self):
-        if  self.__zero_time__ + self.__seconds_passed__ <= time.time():
-            self.__seconds_passed__ += self.__sec__
+        if self.__zero_time + self.__seconds_passed <= time.time():
+            self.__seconds_passed += self.__sec
             return True
         return False
 
-root = CTKi.CTk()
+
+root = ctki.CTk()
+
 
 def create_list_melodies():
     result = []
-    for root, dirs, files in walk(c.MELODIES):
+    for root_, dirs, files in walk(c.MELODIES):
         for file in files:
-            if path.splitext( file )[1] == '.mp3' or path.splitext( file )[1] == '.wav':
+            if path.splitext(file)[1] == '.mp3' or path.splitext(file)[1] == '.wav':
                 result.append(file)
 
     return result
+
 
 #
 #   convert numbers to text and change masculine to feminine
 #   Параметры:
 #   tm      - number of minutes or seconds
 #   min_sec - unit of measure tm (minutes or seconds)
+
+
 def time_to_speach(tm, min_sec):
     unit_of_measure = {0: (' минут',  ' секунд'),
-            1: (' минута', ' секунда'),
-            2: (' минуты', ' секунды')}
+                       1: (' минута', ' секунда'),
+                       2: (' минуты', ' секунды')}
 
-    text = num2words( tm, lang='ru' ).replace('один', 'одна').replace('два', 'две')\
+    text = num2words(tm, lang='ru').replace('один', 'одна').replace('два', 'две') \
         .replace('дведцать', 'двадцать')
     if tm % 10 == 1 and tm != 11:
         text += unit_of_measure[1][min_sec]
-    elif 1 < tm % 10 < 5 and tm != 12 and tm != 13 and tm != 14:
+    elif 1 < tm % 10 < 5 and not (11 < tm < 15):
         text += unit_of_measure[2][min_sec]
     else:
         text += unit_of_measure[0][min_sec]
 
     return text
 
+
 #
 #   we form the text and give it for sounding
 def speach(sec_time_total_left):
-
     text_min = sec_time_total_left // 60
     text_sec = sec_time_total_left % 60
 
@@ -84,49 +89,51 @@ def speach(sec_time_total_left):
     elif text_sec == 0:
         text_of_speach = time_to_speach(text_min, c.MINUTES)
     else:
-        text_of_speach = time_to_speach(text_min, c.MINUTES) + ',' + time_to_speach(text_sec, SECUNDES)
+        text_of_speach = time_to_speach(text_min, c.MINUTES) + ',' + time_to_speach(text_sec, c.SECUNDES)
     s = gTTS(text_of_speach, lang='ru')
     if path.isfile(c.SPEECH_FILE):
         remove(c.SPEECH_FILE)
-    s.save(c.SPEECH_FILE )
-    Thread( target=playsound, args=(c.SPEECH_FILE,), daemon=True ).start()
+    s.save(c.SPEECH_FILE)
+    Thread(target=playsound, args=(c.SPEECH_FILE,), daemon=True).start()
+
+
 #
 #   The actual operation of the timer
 def timer_operation():
-
-    timer_tk.button_start.configure(state=CTKi.DISABLED, fg_color=c.DISABLE_COLOR)
+    timer_tk.button_start.configure(state=ctki.DISABLED, fg_color=c.DISABLE_COLOR)
     int_number_of_stage = timer_tk.create_int_number_of_stage()
-    sec_time_total      = timer_tk.create_sec_time_stage() * int_number_of_stage
-    sec_time_stage      = timer_tk.create_sec_time_stage()
+    sec_time_total = timer_tk.create_sec_time_stage() * int_number_of_stage
+    sec_time_stage = timer_tk.create_sec_time_stage()
 
-    timer_tk.label_time_stage_left.configure( text=timer_tk.sec_to_str(sec_time_stage),
-                                              text_color='SpringGreen3' )
-    timer_tk.label_time_total_left.configure( text=timer_tk.sec_to_str(sec_time_total),
-                                              text_color='SpringGreen3' )
+    timer_tk.label_time_stage_left.configure(text=timer_tk.sec_to_str(sec_time_stage),
+                                             text_color='SpringGreen3', font=('Arial Black', 18))
+    timer_tk.label_time_total_left.configure(text=timer_tk.sec_to_str(sec_time_total),
+                                             text_color='SpringGreen3', font=('Arial Black', 18))
     root.update()
     sec_time_total_left = sec_time_total
-    my_sleep = MySleep( sec=1 )
+    my_sleep = MySleep(sec=1)
     for _ in range(int_number_of_stage):
         sec_time_stage_left = sec_time_stage
         for _ in range(sec_time_stage):
             if timer_tk.signal_end_of_programm:
                 sys_exit()
-            while (not my_sleep.end_of_cycle()):
-                time.sleep( 0.001 )
+            while not my_sleep.end_of_cycle():
+                time.sleep(0.001)
             sec_time_total_left -= 1
             sec_time_stage_left -= 1
             timer_tk.label_time_total_left.configure(
-                text=timer_tk.sec_to_str( sec_time_total_left ) )
+                text=timer_tk.sec_to_str(sec_time_total_left))
             timer_tk.label_time_stage_left.configure(
-                text=timer_tk.sec_to_str( sec_time_stage_left ) )
+                text=timer_tk.sec_to_str(sec_time_stage_left))
             root.update()
 
-        speach( sec_time_total_left )
+        speach(sec_time_total_left)
 
     melody_file = c.MELODIES + '/' + timer_tk.ending_melody
     if path.isfile(melody_file):
-        Thread( target=playsound, args=(melody_file,) ).start()
+        Thread(target=playsound, args=(melody_file,)).start()
     timer_tk.stop()
+
 
 #
 #   Getting Started and Filling in Initial Values
@@ -134,19 +141,19 @@ timer_tk = TimerTK(root, timer_operation)
 
 try:
     with open(c.SETTING_FILE, 'r') as file_setting:
-        time_stage_m           = file_setting.readline().strip()
-        time_stage_s           = file_setting.readline().strip()
-        number_of_stage        = file_setting.readline().strip()
+        time_stage_m = file_setting.readline().strip()
+        time_stage_s = file_setting.readline().strip()
+        number_of_stage = file_setting.readline().strip()
         timer_tk.ending_melody = file_setting.readline().strip()
 
 except FileNotFoundError:
-    time_stage_m               = '00'
-    time_stage_s               = '03'
-    number_of_stage            = '1'
-    timer_tk.ending_melody     = ''
+    time_stage_m = '00'
+    time_stage_s = '03'
+    number_of_stage = '1'
+    timer_tk.ending_melody = ''
 
 timer_tk.list_melodies = create_list_melodies()
-root.protocol( 'WM_DELETE_WINDOW', timer_tk.stop )
+root.protocol('WM_DELETE_WINDOW', timer_tk.stop)
 timer_tk.create_widgets()
 
 timer_tk.entry_time_stage_m.set(time_stage_m)
@@ -154,4 +161,3 @@ timer_tk.entry_time_stage_s.set(time_stage_s)
 timer_tk.entry_number_of_stage.set(number_of_stage)
 
 root.mainloop()
-
